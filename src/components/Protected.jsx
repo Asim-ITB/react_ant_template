f9d6afbb29bc4fc0eded5c49ffdd6d73b2aa2381
Asim-ITB <asim.itbridge@gmail.com>
2024-04-25 21:02:39 +0545

    changed protected route logic

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -1,22 +1,10 @@
 /* eslint-disable react/prop-types */
-// import { useQuery } from "@tanstack/react-query";
 import { Navigate } from "react-router-dom";
-// import { user } from "../Apis/TestApi";
+import { useAuthStore } from "../stores/authStore";
+
 function PrivateRoute({ children }) {
-  const tokenString = localStorage.getItem("token");
-  const isLoggedIn = !!tokenString;
-  // const { data, error } = useQuery({
-  //   queryKey: ["user"],
-  //   queryFn: user,
-  // });
-  // if (data) {
-  //   localStorage.setItem("user", data);
-  // }
-  // if (error) {
-  //   console.log(error);
-  // }
-  const isAuthenticated = isLoggedIn;
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  const { userData } = useAuthStore();
+  return userData.isLoggedIn ? children : <Navigate to="/login" />;
 }
 
 export default PrivateRoute;
